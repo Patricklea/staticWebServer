c9c30bd9260c159c51787dcff239af5a99320e81
pc <575720013@qq.com>
2018-09-02 22:27:30 +0800

    利用promisify改造异步流程

--- src/helper/route.js
+++ src/helper/route.js
@@ -8,24 +8,24 @@ const readdir = promisify(fs.readdir);
  * 改造后的方法前面一定要用 await 接收，否则只会拿到一个Promise对象；
  */
 module.exports = async function (req, res, filePath) {
-    try {
-        const stats = await stat(filePath);
-        if (stats.isFile()) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            fs.createReadStream(filePath).pipe(res);
+	try {
+		const stats = await stat(filePath);
+		if (stats.isFile()) {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			fs.createReadStream(filePath).pipe(res);
 
-        } else if (stats.isDirectory()) {
-            const files = await readdir(filePath);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(files.join(',,,')); 
-        }        
-    } catch (ex) {
-        console.error(`ex:`,ex);
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`${filePath} is not a directory or file.`);
-    }
+		} else if (stats.isDirectory()) {
+			const files = await readdir(filePath);
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end(files.join(',,,')); 
+		}        
+	} catch (ex) {
+		console.error(`ex:`,ex);
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end(`${filePath} is not a directory or file.`);
+	}
 }
 
