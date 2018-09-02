7461f633d6f0fc6d8f25ceb8ec421b9e23703bb8
pc <575720013@qq.com>
2018-09-02 20:34:21 +0800

    `

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ const conf = require('./config/defaultConf');
 const server = http.createServer((req,res) => {
 
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<body>');
-    res.write('Hello Htttttp');
-    res.write('</body>');
-    res.write('</html>');
-    res.end();
+	res.statusCode = 200;
+	res.setHeader('Content-Type','text/html');
+	res.write('<html>');
+	res.write('<body>');
+	res.write('Hello Htttttp');
+	res.write('</body>');
+	res.write('</html>');
+	res.end();
 })
 
 server.listen(conf.port,conf.host,()=>{
