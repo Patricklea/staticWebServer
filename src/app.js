162ee0240a8fa579f3ac20aa2c976587aa9b36f0
pc <575720013@qq.com>
2018-09-02 21:18:05 +0800

    同上

--- src/app.js
+++ src/app.js
@@ -5,34 +5,34 @@ const chalk = require('chalk');
 const conf = require('./config/defaultConf');
 
 const server = http.createServer((req,res) => {
-    // 拼接请求的文件所在的服务器的目录
-    const filePath = path.join(conf.root,req.url);
-    /**
+	// 拼接请求的文件所在的服务器的目录
+	const filePath = path.join(conf.root,req.url);
+	/**
      * fs.stat用于检查指定路径的文件状态
      * 第一个参数是路径，第二个是回调
      * 回调的第二个参数status是一个fs.Stats对象
      */
-    fs.stat(filePath,(err,stats) => {
-        if (err) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(`${filePath} is not a directory or file.`);
-            return;
-        }
+	fs.stat(filePath,(err,stats) => {
+		if (err) {
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/plain');
+			res.end(`${filePath} is not a directory or file.`);
+			return;
+		}
 
-        if (stats.isFile()) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            fs.createReadStream(filePath).pipe(res);
+		if (stats.isFile()) {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'text/plain');
+			fs.createReadStream(filePath).pipe(res);
 
-        } else if (stats.isDirectory()) {
-            fs.readdir(filePath,(err,files) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(files.join('---'));
-            })
-        }
-    })
+		} else if (stats.isDirectory()) {
+			fs.readdir(filePath,(err,files) => {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/plain');
+				res.end(files.join('---'));
+			})
+		}
+	})
 
 })
 
--- src/config/defaultConf.js
+++ src/config/defaultConf.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // 执行项目启动时所在的路径
+	// 执行项目启动时所在的路径
 	root: process.cwd(),
 	host: '127.0.0.1',
 	port: 8888
