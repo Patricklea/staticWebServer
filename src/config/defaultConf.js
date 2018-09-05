7d681c3e402e8edd3cf1c328351ed1b818e41ce9
pc <575720013@qq.com>
2018-09-05 09:02:48 +0800

    对指定类型文件进行压缩后返回

--- src/config/defaultConf.js
+++ src/config/defaultConf.js
@@ -2,6 +2,6 @@ module.exports = {
 	// 执行项目启动时所在的路径
 	root: process.cwd(),
 	host: '127.0.0.1',
-    port: 8888,
-    compress: /\.(html|js|css|md)/,
+	port: 8888,
+	compress: /\.(html|js|css|md)/,
 }
--- src/helper/route.js
+++ src/helper/route.js
@@ -26,12 +26,12 @@ module.exports = async function (req, res, filePath) {
 			const contentType = mime(filePath);
 			res.statusCode = 200;
 			res.setHeader('Content-Type', `${contentType};charset=utf-8`);
-            let rs = fs.createReadStream(filePath);
-            // 匹配指定类型的文件进行压缩
-            if (filePath.match(conf.compress)){
-                rs = compress(rs,req,res)
-            }
-            rs.pipe(res);
+			let rs = fs.createReadStream(filePath);
+			// 匹配指定类型的文件进行压缩
+			if (filePath.match(conf.compress)){
+				rs = compress(rs,req,res)
+			}
+			rs.pipe(res);
 
 		} else if (stats.isDirectory()) {
 			const files = await readdir(filePath);
