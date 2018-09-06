87117d7e0898289777ff39003a2358c022d562ce
pc <575720013@qq.com>
2018-09-06 15:43:18 +0800

    通过yargs模块，可以在命令行中自定义项目运行的某些配置，例如主机、端口号等，运行node src/index.js -p 9999便可在9999启动项目

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ const yargs = require('yargs');
 const Server = require('./app');
 
 const argv = yargs
-    .usage('anydoor [options]')
-    .option('p', {
-        alias: 'port',
-        describe: '端口号',
-        defult: 8888
-    })
-    .option('h', {
-        alias: 'hostname',
-        describe: 'host',
-        defult: '127.0.0.1'
-    })
-    .option('d', {
-        alias: 'root',
-        describe: 'root path',
-        defult: process.cwd()
-    })
-    .version()
-    .alias('v', 'versionn')
-    .help()
-    .argv;
+	.usage('anydoor [options]')
+	.option('p', {
+		alias: 'port',
+		describe: '端口号',
+		defult: 8888
+	})
+	.option('h', {
+		alias: 'hostname',
+		describe: 'host',
+		defult: '127.0.0.1'
+	})
+	.option('d', {
+		alias: 'root',
+		describe: 'root path',
+		defult: process.cwd()
+	})
+	.version()
+	.alias('v', 'versionn')
+	.help()
+	.argv;
 
 
 const server = new Server(argv);
